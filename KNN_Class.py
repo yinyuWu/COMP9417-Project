2165726cbdb0bd3f166567172585fb25225b2cac
yinyuWu <2936932419@qq.com>
2019-07-04 19:58:32 +1000

    Add a template of knn for classification numeric prediction respectively

---
+++ KNN_Class.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# knn for classification
+
+class KNN_Class:
+    def __init__(self, parameter_list):
+        pass
+    
+    def Manhattan(self, parameter_list):
+        pass
+    
+    def Euclidean(self, parameter_list):
+        pass
+    
+    def predict(self, parameter_list):
+        pass
