f82f8f6a52bc80eb2ae001d5aa0cb94dba2e4083
marvin chui <marvinchui@gmail.com>
2019-07-06 19:44:23 +1000

    Updated euclidean distance implementation

--- distance.py
+++ distance.py
@@ -1,6 +1,7 @@
 import numpy as np
 def Manhattan(x1, x2):
     return np.sum(np.abs(x1-x2)[0])
-    
+
 def Euclidean(x1, x2):
-    return np.sqrt(np.sum((x1-x2)**2))
+    diff = x2 - x1
+    return np.sqrt(np.dot(diff, diff))
