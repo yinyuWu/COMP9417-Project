b3929260430383023e8a925a62f7427f067ed95a
Nadine Nguyen <naydeen.nguyen@gmail.com>
2019-07-06 19:19:50 +1000

    working but inefficient knn for numeric values

--- KNN_Numeric.py
+++ KNN_Numeric.py
@@ -1,12 +1,15 @@
 import numpy as np
 from scipy.io import arff
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # knn for numeric prediction
 
-class KNN_Class:
-    def __init__(self, data):
-        self.data = data
+class KNN_Numeric:
+    def __init__(self, x_data, labels, k_neighbours):
+        self.x_data = x_data
+        self.labels = labels
+        self.k_neighbours = k_neighbours
     
     def Manhattan(self, x1, x2):
         return np.sum(np.abs(x1-x2)[0])
@@ -14,23 +17,53 @@ class KNN_Class:
     def Euclidean(self, x1, x2):
         return np.sqrt(np.sum((x1-x2)**2))
     
-    def predict(self, parameter_list):
-        pass
+    def predict(self, ux):
+        # Find nearest k neighbours by Euclidean/Manhattan distance
+        n = self.x_data.shape[0]    
+        dists = np.zeros((n, 2))
+        for i in range(n):                                              
+            dists[i][0] = self.Euclidean(self.x_data[i], ux)
+            dists[i][1] = self.labels[i]
 
-def remove_missing(data):
-    return data.dropna(how='any')
+        sorted_dists = dists[np.argsort(dists[:,0])]      # Sort by distances which is 1st column              
+        neighbours = sorted_dists[:self.k_neighbours]     # Take only the closest k neighbours
+        return np.mean(neighbours[:,1])                   # Return mean of 2nd column which is labels of neighbours
 
-def remove_categorical(data):
-    return data.drop(['make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 
+def Test_KNN_Numeric(x_data, labels):
+    # Scale numeric features so they are between 0-1
+    scaler = MinMaxScaler()
+    scaled_x_data = scaler.fit_transform(x_data)
+
+    x_test = scaled_x_data[0]       # first entry is test data
+    y_test = labels[0]
+    x_train = scaled_x_data[1:]     # the rest is training
+    y_train = labels[1:]
+
+    knn = KNN_Numeric(x_train, y_train, 7)
+    print(f'Predicted Price: {knn.predict(x_test)}')
+    print(f'Actual Price: {y_test}')
+    
+
+def main():
+    # Load data from autos.aff
+    data_set = arff.loadarff('autos.arff')
+    data = pd.DataFrame(data_set[0])
+    
+    # Remove any categorical labels (Note: temporary as we can encode these categorical labels later)
+    data.drop(['make', 'fuel-type', 'aspiration', 'num-of-doors', 'body-style', 
                 'drive-wheels', 'engine-location', 'engine-type', 'num-of-cylinders', 
                 'fuel-system'], axis=1, inplace=True)
 
-def main():
-    data_set = arff.loadarff('autos.arff')
-    data = pd.DataFrame(data_set[0])
+    # Remove missing values
+    filtered = data.dropna()
+    print(filtered.shape)
 
-    # Remove missing values and remove any categorical labels (Note: temporary as we can encode these categorical labels later)
-    filtered = remove_categorical(remove_missing(data)).to_numpy()  
+    # Separate labels & x-data
+    labels = filtered['price']
+    x_data = filtered.drop('price', axis=1)
+
+    # TEMPORARY TEST FOR KNN NUMERIC
+    Test_KNN_Numeric(x_data, labels.to_numpy())
 
     #Manhattan_Test(data)
     #Euclidean_Test(data)
