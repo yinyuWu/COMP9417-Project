8b97e300d8b5a7c0a194925572140aa02a4b0c0c
Nadine Nguyen <naydeen.nguyen@gmail.com>
2019-07-31 17:33:49 +1000

    target function and dataset

--- target.py
+++ target.py
@@ -2,77 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from KNN_Class import KNN_Class, cross_validation
 
-""" Determining range of target function depending on the class probability 
-- Any data point that results in the function > 0, it is in class 1
-- Otherwise they are classified as 2. 
-    E.g. function    =  sin(x1) + sin(x2) + sin(x3) ...   
-        data point   = [1,1,1,1,1]        > 0   ==> class 1 
-        data point   = [-1,-1,-1,-1,-1]  <= 0   ==> class 2 
-"""    
-def generate_target_function(num_features, split=0.5):
-    if split == 0:
-        range = (-np.pi/2, 0)         # class 2 only
-    elif split == 1:
-        range = (0 , np.pi/2)         # class 1 only 
-    else:
-        range = (-np.pi/2, np.pi/2)   # 50-50 split between class 1 and class 2
-    
-    def evaluate(x):
-        result = 0
-        for i in x:
-            result += np.sin(i)        # y = sin(x1) + sin(x2) + .... => ~ num_features * sin(x)
-        return result
+from scipy.stats import multivariate_normal
 
-    return evaluate, range
+class TargetFunction:       # multivariate normal distributions 
+    def __init__(self, mean, cov, n_dimensions, class_label):
+        self.mean = mean 
+        self.cov = cov 
+        assert(len(mean) == n_dimensions)
+        self.n_dimensions = n_dimensions
+        self.class_label = class_label
 
-""" Generate 'n' number of samples with 'f' features in a given range """
-def generate_dataset(range, n, f):    
+    def generate_point(self):
+        return np.random.multivariate_normal(self.mean, self.cov)
+
+    def get_probability(self, x):
+        return multivariate_normal.pdf(x, mean=self.mean, cov=self.cov)
+
+""" Generate 'n' number of samples given p prior probability of class 0, and two target functions """
+def generate_dataset(p, n, target_f1, target_f2):    
     np.random.seed(124934589)
-    min, max = range
+    class_assign = np.random.uniform(low=0,high=1,size=n)
+    class_0 = []
+    class_1 = []
+    for c in class_assign:
+        if c < p:
+            class_0.append(target_f1.generate_point())
+        else:
+            class_1.append(target_f2.generate_point())
+    return np.asarray(class_0), np.asarray(class_1)
     
-    # dataset = np.random.uniform(low=min, high=max, size=(n,f))
-    # return dataset
-
-    skew = (max-min)/5                  # add misclassifications to dataset
-    class_1 = np.random.uniform(low=min+skew, high=max, size=(n, f))
-    class_2 = np.random.uniform(low=min, high=max-skew, size=(n, f))
-    return class_1, class_2
-    
-
-def get_accuracy(dataset, func, predicted_label):
-    correct = 0 
-    total = 0
-    for point in dataset:
-        total += 1
-        if func(point) > 0 and predicted_label == 1:
+def calc_bayes_error(x_data, labels, target_f1, target_f2):
+    correct = 0
+    for x, y in zip(x_data, labels):
+        p_target1 = target_f1.get_probability(x)
+        p_target2 = target_f2.get_probability(x)
+        if p_target1 > p_target2:
+            predicted = target_f1.class_label 
+        else:
+            predicted = target_f2.class_label 
+        if int(predicted) == int(y): 
             correct += 1
-        elif func(point) <= 0 and predicted_label == 2:
-            correct += 1 
-    return correct/total
-        
-
-def get_error(dataset, func, predicted_label):
-    wrong = 0 
-    total = 0 
-    for point in dataset:
-        total += 1 
-        if func(point) > 0 and predicted_label == 2:
-            wrong += 1 
-        elif func(point) <= 0 and predicted_label == 1:
-            wrong += 1
-    return wrong/total 
-
-def get_bayes_error(error, probability):
-    min_error = 0
-    for err, p in zip(error, probability):
-        min_error += err*p 
-    return min_error
+    acc = correct/len(labels)
+    print(f'Accuracy: {correct}/{len(labels)}')
+    return 1 - acc
 
 """ Adding labels to data & shuffling for use in KNN """
-def make_useable_dataset(func, class_1, class_2):
+def make_useable_dataset(c1, label1, c2, label2):
     # add labels to the dataset
-    data_1 = np.insert(class_1, 0, 1, axis=1)
-    data_2 = np.insert(class_2, 0, 2, axis=1)
+    data_1 = np.insert(c1, 0, label1, axis=1)
+    data_2 = np.insert(c2, 0, label2, axis=1)
     
     # combine them into one & shuffle 
     data = np.concatenate((data_1, data_2), axis=0)
@@ -81,51 +59,44 @@ def make_useable_dataset(func, class_1, class_2):
     x_data = data[:,1:]
     return x_data, labels
 
+def generate_mean(n):
+    return np.random.uniform(0,1,n)
+
+def generate_covariance_matrix(n):
+    r = np.random.random((n,n))
+    matrix = np.dot(r, r.transpose())
+    return matrix
+
 if __name__=='__main__':
-
     p = 0.5
-    num_f = 5
     num_samples = 160
+    num_f = 10
 
-    func, range = generate_target_function(num_f, p)
-    print('If point is greater than this function in all dimensions then it is in class 1, otherwise class 2')
-    print(f'function: {func}')
+    mean0 = generate_mean(num_f)
+    cov0 = generate_covariance_matrix(num_f)
+    target0 = TargetFunction(mean0, cov0, num_f, 0)
 
-    print('------------------------------')
-    print(f'Generating skewed data based on this target function {p} probability between the two classes...')
-    # data = generate_dataset(range, num_samples, num_f)
-    # print(data.shape)
-    # print(data[:5])
-
-    class_1, class_2 = generate_dataset(range, num_samples, num_f)
-    print(class_1.shape, class_2.shape)
-    print(f'First 5 of data points of class 1: \n{class_1[:5]}')
-    acc_1 = get_accuracy(class_1, func, 1)
-    print(f'Maximum accuracy using this dataset for class 1: {acc_1}')
-
-    print(f'First 5 of data points of class 2: \n{class_2[:5]}')
-    acc_2 = get_accuracy(class_2, func, 2)
-    print(f'Maximum accuracy using this dataset for class 2: {acc_2}')
-    
+    mean1 = generate_mean(num_f)
+    cov1 = generate_covariance_matrix(num_f)
+    target1 = TargetFunction(mean1, cov1, num_f, 1)
 
     print('------------------------------')
-    errors = []
-    probabilities = []
-    errors.append(get_error(class_1, func, 1))
-    print(f'Probability of classifying class 2 as class 1: {errors[0]}')
-    probabilities.append(p)
-    errors.append(get_error(class_2, func, 2))
-    print(f'Probability of classifying class 1 as class 2: {errors[1]}')
-    probabilities.append(1-p)
-    bayes_error = get_bayes_error(errors, probabilities)
-    print(f'Bayes Error of this dataset is: {bayes_error:.2f}')
+    print(f'Generating data based on target function {p} probability between the two classes...')
 
+    class_0, class_1 = generate_dataset(p, num_samples, target0, target1)
+    print(class_0.shape, class_1.shape)
+    print(f'First 2 data points of class 0: \n{class_0[:2]}')
+    print(f'First 2 data points of class 1: \n{class_1[:2]}')
+
+    print('------------------------------')
+    print('Creating data set')
+    x_data, labels = make_useable_dataset(class_0, 0, class_1, 1)
+    print(f'First 2 data points of x_data:\n{x_data[:2]}')
+    print(f'First 2 data points labels:\n{labels[:2]}')
+
+    err = calc_bayes_error(x_data, labels, target0, target1)
+    print(f'Estimated Bayes error rate on this dataset: {err}')
 
     print('------------------------------')
     print('Using dataset with KNN...')
-    x_data, labels = make_useable_dataset(func=func, class_1=class_1, class_2=class_2)
     cross_validation(x_data, labels, KNN_Class())
-
-
-
-    
